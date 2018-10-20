0a09007c59d8144021f8765fef62399dc0a196f1
rifatduru7 <43688438+rifatduru7@users.noreply.github.com>
2018-10-21 01:10:03 +0300

    Ürünler tablosunun html yapısı oluşturuldu.Sil ve Düzenle butonları eklendi

--- panel/application/views/product_v/content.php
+++ panel/application/views/product_v/content.php
@@ -1,1 +1,17 @@
-Ürün Listesi...
+<div class="row">
+    <div class="col-md-12">
+        <div class="widget p-lg">
+            <h4 class="m-b-lg">Striped rows</h4>
+            <p class="m-b-lg docs">
+                Use <code>.table-striped</code> to add zebra-striping to any table row within the <code>&lt;tbody&gt;</code>.
+            </p>
+
+            <table class="table table-striped">
+                <tr><th>#</th><th>First Name</th><th>Last Name</th><th>Username</th></tr>
+                <tr><td>1</td><td>Mark</td><td>Otto</td><td>@mdo</td></tr>
+                <tr><td>2</td><td>Jacob</td><td>Thornton</td><td>@fat</td></tr>
+                <tr><td>3</td><td>Larry</td><td>the Bird</td><td>@twitter</td></tr>
+            </table>
+        </div><!-- .widget -->
+    </div><!-- END column -->
+</div>
--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,66 @@
-urun listesi content....
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">Ürün Listesi</h4>
+        </div>
+        <div class="col-md-12">
+        <div class="widget p-lg">
+            <table class="table table-hover table-striped ">
+                <thead>
+                <th>ID</th>
+                <th>url</th>
+                <th>Başlık</th>
+                <th>Açıklama</th>
+                <th>Durumu</th>
+                <th>İşlem</th>
+                </thead>
+                <tr>
+                <tr>
+                <td>1</td>
+                <td>monitor_askisi</td>
+                <td>Monitör Askısı</td>
+                <td>Monitor Askısı Mediamarkt</td>
+                <td>1/0</td>
+                <td>
+                    <a href="#" type="button" class="btn btn-danger btn-sm btn-outline"><i class="fa fa-trash"></i> Sil </a>
+                    <a href="#" type="button" class="btn btn-info btn-sm btn-outline"><i class="fa fa-pencil"></i> Düzenle </a>
+                </td>
+                </tr>
+                <tr>
+                <td>2</td>
+                <td>monitor_askisi</td>
+                <td>Monitör Askısı</td>
+                <td>Monitor Askısı Mediamarkt</td>
+                <td>1/0</td>
+                <td>
+                    <a href="#" type="button" class="btn btn-danger btn-sm btn-outline"><i class="fa fa-trash"></i> Sil </a>
+                    <a href="#" type="button" class="btn btn-info btn-sm btn-outline"><i class="fa fa-pencil"></i> Düzenle </a>
+                </td>
+
+                </tr>
+
+                <tr>
+                <td>3</td>
+                <td>monitor_askisi</td>
+                <td>Monitör Askısı</td>
+                <td>Monitor Askısı Mediamarkt</td>
+                <td>1/0</td>
+                <td>
+                    <a href="#" type="button" class="btn btn-danger btn-sm btn-outline"><i class="fa fa-trash"></i> Sil </a>
+                    <a href="#" type="button" class="btn btn-info btn-sm btn-outline"><i class="fa fa-pencil"></i> Düzenle </a>
+                </td>
+                </tr>
+                <td>4</td>
+                <td>monitor_askisi</td>
+                <td>Monitör Askısı</td>
+                <td>Monitor Askısı Mediamarkt</td>
+                <td>1/0</td>
+                <td>
+                    <a href="#" type="button" class="btn btn-danger btn-sm btn-outline"><i class="fa fa-trash"></i> Sil </a>
+                    <a href="#" type="button" class="btn btn-info btn-sm btn-outline"><i class="fa fa-pencil"></i> Düzenle </a>
+                </td>
+                </tr>
+                </tbody>
+            </table>
+        </div><!-- .widget -->
+    </div><!-- END column -->
+</div>

6ea0948a7ea88696465de6bf8617a4fa2cddd477
rifatduru7 <43688438+rifatduru7@users.noreply.github.com>
2018-10-21 13:24:03 +0300

    Tabloya switch buton,yeni ekle butonu ve alert alanı eklendi.

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,9 +1,18 @@
 <div class="row">
     <div class="col-md-12">
-        <h4 class="m-b-lg">Ürün Listesi</h4>
+        <h4 class="m-b-lg">
+            Ürün Listesi
+            <a href="#" class="btn btn-info btn-outline btn-sm pull-right" ><i class="fa fa-plus"></i>Yeni Ekle</a>
+        </h4>
         </div>
+
         <div class="col-md-12">
         <div class="widget p-lg">
+            <div class="alert alert-danger" role="alert">
+                <strong>Uyarı !</strong>
+                <span>Burada herhangi bir kayıt bulunamamıştır.Yeni eklemek için lütfen</span>
+                <a href="#" class="alert-link">Tıklayın.</a>
+            </div>
             <table class="table table-hover table-striped ">
                 <thead>
                 <th>ID</th>
@@ -19,7 +28,9 @@
                 <td>monitor_askisi</td>
                 <td>Monitör Askısı</td>
                 <td>Monitor Askısı Mediamarkt</td>
-                <td>1/0</td>
+                <td>
+                    <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+                </td>
                 <td>
                     <a href="#" type="button" class="btn btn-danger btn-sm btn-outline"><i class="fa fa-trash"></i> Sil </a>
                     <a href="#" type="button" class="btn btn-info btn-sm btn-outline"><i class="fa fa-pencil"></i> Düzenle </a>
@@ -30,7 +41,10 @@
                 <td>monitor_askisi</td>
                 <td>Monitör Askısı</td>
                 <td>Monitor Askısı Mediamarkt</td>
-                <td>1/0</td>
+                <td>
+                    <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+
+                </td>
                 <td>
                     <a href="#" type="button" class="btn btn-danger btn-sm btn-outline"><i class="fa fa-trash"></i> Sil </a>
                     <a href="#" type="button" class="btn btn-info btn-sm btn-outline"><i class="fa fa-pencil"></i> Düzenle </a>
@@ -43,7 +57,9 @@
                 <td>monitor_askisi</td>
                 <td>Monitör Askısı</td>
                 <td>Monitor Askısı Mediamarkt</td>
-                <td>1/0</td>
+                <td>
+                    <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+                </td>
                 <td>
                     <a href="#" type="button" class="btn btn-danger btn-sm btn-outline"><i class="fa fa-trash"></i> Sil </a>
                     <a href="#" type="button" class="btn btn-info btn-sm btn-outline"><i class="fa fa-pencil"></i> Düzenle </a>
@@ -53,7 +69,9 @@
                 <td>monitor_askisi</td>
                 <td>Monitör Askısı</td>
                 <td>Monitor Askısı Mediamarkt</td>
-                <td>1/0</td>
+                <td>
+                    <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+                </td>
                 <td>
                     <a href="#" type="button" class="btn btn-danger btn-sm btn-outline"><i class="fa fa-trash"></i> Sil </a>
                     <a href="#" type="button" class="btn btn-info btn-sm btn-outline"><i class="fa fa-pencil"></i> Düzenle </a>

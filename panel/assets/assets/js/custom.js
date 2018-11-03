b3dc38f680db34c288a415da0d7ddb874f96e2b2
rifatduru7 <43688438+rifatduru7@users.noreply.github.com>
2018-11-04 02:06:58 +0300

    progress bar güncellendi

--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,9 +1,21 @@
 $(document).ready(function () {
-    swal({
-        title: 'Error',
-        text: 'Devam Etmek istermisiniz?',
-        type:'error',
-        confirmButtonText:'Cool'
+    $(".remove-btn").click(function (e) {
+      var  $data_url = $(this).data("url");
 
+        swal({
+            title: 'Emin misiniz?',
+            text: "Silinen veriler geri getirilemeyebilir!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            cancelButtonText : 'İptal',
+            confirmButtonText: 'Evet, sil!'
+        }).then(function(result) {
+            if (result.value) {
+                window.location.href =$data_url;
+            }
+        });
     })
+
 })

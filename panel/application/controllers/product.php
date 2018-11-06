901a0219a36efa7cb3e6783982aada0b0cedc0cc
rifatduru7 <43688438+rifatduru7@users.noreply.github.com>
2018-11-07 01:05:07 +0300

    upload işleminin tabloya aktarılması

--- panel/application/controllers/product.php
+++ panel/application/controllers/product.php
@@ -9,6 +9,8 @@ class Product extends CI_Controller
         parent::__construct();
         $this->viewFolder = "product_v";
         $this->load->model("product_model");
+        $this->load->model("product_image_model");
+
     }
 
     public function index(){
@@ -223,7 +225,7 @@ class Product extends CI_Controller
         $viewData = new stdClass();
         $viewData->viewFolder = $this->viewFolder;
         $viewData->subViewFolder = "image";
-        
+
         $viewData->item = $this->product_model->get(
             array(
                 "id"    => $id
@@ -233,20 +235,35 @@ class Product extends CI_Controller
         $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index" ,$viewData);
 
     }
-    public function image_upload(){
-        $config["allowed_types"] = "jpeg|jpg|png|gif";
-        $config["upload_path"] = "uploads/$this->viewFolder/";
-        $this->load->library("upload" ,$config);
+    public function image_upload($id){
+
+        $config["allowed_types"] = "jpg|jpeg|png";
+        $config["upload_path"]   = "uploads/$this->viewFolder/";
+
+        $this->load->library("upload", $config);
 
         $upload = $this->upload->do_upload("file");
 
         if($upload){
 
-            echo "islem basarili";
-        }else{
+            $uploaded_file = $this->upload->data("file_name");
 
+            $this->product_image_model->add(
+                array(
+                    "img_url"       => $uploaded_file,
+                    "rank"          => 0,
+                    "isActive"      => 1,
+                    "isCover"       => 0,
+                    "createdAt"     => date("Y-m-d H:i:s"),
+                    "product_id"    => $id
+                )
+            );
+
+
+        } else {
             echo "islem basarisiz";
         }
 
     }
+
 }

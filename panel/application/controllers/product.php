6e60c5486c67d6aec5016a9f2137b5d20a7a3d83
rifatduru7 <43688438+rifatduru7@users.noreply.github.com>
2018-11-09 01:41:29 +0300

    Yüklenen resimlerin Adları Seo uyumlu olarak yüklenmesini sağlayacak jenerik yapı oluşturuldu.

--- panel/application/controllers/product.php
+++ panel/application/controllers/product.php
@@ -244,8 +244,11 @@ class Product extends CI_Controller
     }
     public function image_upload($id){
 
+        $file_name = convertToSEO(pathinfo($_FILES["file"]["name"], PATHINFO_FILENAME)) . "." . pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION);
+
         $config["allowed_types"] = "jpg|jpeg|png";
         $config["upload_path"]   = "uploads/$this->viewFolder/";
+        $config["file_name"]     = $file_name;
 
         $this->load->library("upload", $config);
 
